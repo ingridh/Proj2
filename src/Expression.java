2f1ca917559251f7c7519b91ed791609d2cfa8a1
kellygoodman <kellyg3000@gmail.com>
2013-07-17 20:21:57 -0700

    Update Expression.java

--- src/Expression.java
+++ src/Expression.java
@@ -1,5 +1,13 @@
 public class Expression {
 
+	private boolean value;	// true of false value of node (can be set by "assume" or by checking logic)
+	private String prop;	// string representation of the proposition or operand
+	
 	public Expression (String s) throws IllegalLineException {
+		
+	}
+	
+	public void set(boolean b) {
+		value = b;
 	}
 }
