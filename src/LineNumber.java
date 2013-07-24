348763d0c44010930506bb6d308adf336811ebc3
Jessica K <jessicak@berkeley.edu>
2013-07-24 01:41:13 -0700

    Updated Error Checking

--- src/LineNumber.java
+++ src/LineNumber.java
@@ -4,6 +4,8 @@ public class LineNumber {
 	
 	public ArrayList<Integer> current;
 	
+	boolean iAmDebugging = true;
+	
 	public LineNumber(int s) {
 		current = new ArrayList<Integer>();
 		current.add(s);
@@ -29,31 +31,38 @@ public class LineNumber {
 		}
 	
 	
-	public boolean isLegalLine(String s) {
+	public boolean isLegalLine(String s) throws IllegalLineException {		
 		String[] temp= s.split("\\.");
-		System.out.println(temp.length);
-		
-		if (current.size()<temp.length) {
-			return false;
+		//Correct amount of periods.
+		int counter=0;
+		for (int i=0; i<s.length(); i++) {
+			if (s.charAt(i)=='.') {
+				counter++;
+			}
 		}
-		
+		if (counter!=temp.length-1) {
+			throw new IllegalLineException("Line has too many periods.");
+		}
+		/*if (iAmDebugging) {
+			System.out.println(temp.length);	
+		}*/ if (current.size()<temp.length) {
+			return false; //Line cannot be longer than current.
+		}
 		//Checking that all but the last digit are the same.
 		for (int i=0; i<temp.length-1;i++) {
 			int tempInt = Integer.parseInt(temp[i]);
-			System.out.println(tempInt + "compare" + current.get(i));
-			if (tempInt!=current.get(i)) {
+			/*if (iAmDebugging) {
+				System.out.println(tempInt + "compare" + current.get(i));
+			}*/ if (tempInt!=current.get(i)) {
 				return false; 
 			}
+		} try {
+			return Integer.parseInt(temp[temp.length-1]) < current.get(temp.length-1);	
+		} catch (NumberFormatException i) { //Not integers.
+			throw new IllegalLineException("Line reference can only be numbers.");
 		}
-		int lastcurr = (current.get(temp.length-1));
-		int lastInt = Integer.parseInt(temp[temp.length-1]);
-		System.out.println(lastInt + "last compare" + current.get(temp.length-1));
-		System.out.println("last" + (lastInt <lastcurr) );
-		return (lastcurr<lastInt);	
 	}
-	//What if it's a letter?	
 	
-				
 	public String toString() {
 		String temp = "";
 		//Turns ArrayList into a string with periods.
