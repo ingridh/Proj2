eeed9f0a8d7effc6a347bb46324c77e2afd20a2a
Jessica K <jessicak@berkeley.edu>
2013-07-24 01:41:26 -0700

    More tests added

--- src/LineNumberTest.java
+++ src/LineNumberTest.java
@@ -10,6 +10,39 @@ public class LineNumberTest extends TestCase {
 		assertEquals(a.toString(), "1");
 		LineNumber b = new LineNumber(56);
 		assertEquals(b.toString(), "56");
+
+		
+		//Illegal Lines. 
+		boolean thrown=false;
+		try {
+			a.isLegalLine("~"); //Unacceptable character.
+		} catch (IllegalLineException e) {
+			thrown = true;
+		}
+		assertTrue(thrown);
+				
+		thrown= false;
+		try {
+			a.isLegalLine("hello"); //Letters not allowed.
+		} catch (IllegalLineException e) {
+			thrown = true;
+		}
+		assertTrue(thrown);
+		
+		thrown= false;
+		try {
+			a.isLegalLine("2."); //Extra period.
+		} catch (IllegalLineException e) {
+			thrown = true;
+		}
+		
+		thrown= false;
+		try {
+			a.isLegalLine("2..4"); //Two periods in a row.
+		} catch (IllegalLineException e) {
+			thrown = true;
+		}
+
 	}
 
 	public void testNextLine() {
