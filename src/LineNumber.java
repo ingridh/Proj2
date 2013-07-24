75de50cec61890d40b87d4bd5e95716320e56a63
Jessica K <jessicak@berkeley.edu>
2013-07-23 17:06:41 -0700

    Update on Line

--- src/LineNumber.java
+++ src/LineNumber.java
@@ -8,6 +8,7 @@ public class LineNumber {
 		current = new ArrayList<Integer>();
 		current.add(s);
 	}
+
 	
 	public void findNextLine(String s) {
 		
@@ -26,6 +27,31 @@ public class LineNumber {
 			current.set(current.size()-1, current.get(current.size()-1)+1);
 			}
 		}
+	
+	
+	public boolean isLegalLine(String s) {
+		String[] temp= s.split("\\.");
+		System.out.println(temp.length);
+		
+		if (current.size()<temp.length) {
+			return false;
+		}
+		
+		//Checking that all but the last digit are the same.
+		for (int i=0; i<temp.length-1;i++) {
+			int tempInt = Integer.parseInt(temp[i]);
+			System.out.println(tempInt + "compare" + current.get(i));
+			if (tempInt!=current.get(i)) {
+				return false; 
+			}
+		}
+		int lastcurr = (current.get(temp.length-1));
+		int lastInt = Integer.parseInt(temp[temp.length-1]);
+		System.out.println(lastInt + "last compare" + current.get(temp.length-1));
+		System.out.println("last" + (lastInt <lastcurr) );
+		return (lastcurr<lastInt);	
+	}
+	//What if it's a letter?	
 	
 				
 	public String toString() {
