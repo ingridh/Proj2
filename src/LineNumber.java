54ce7128295dbe099ec9ca9f8df79160533b4845
Jessica K <jessicak@berkeley.edu>
2013-07-25 02:57:59 -0700

    Changed structure slightly.

--- src/LineNumber.java
+++ src/LineNumber.java
@@ -2,20 +2,58 @@ import java.util.*;
 
 public class LineNumber {	
 	
-	public ArrayList<Integer> current;
+	private ArrayList<Integer> current;
 	
 	boolean iAmDebugging = true;
 	
-	public LineNumber(int s) {
+	public LineNumber() {
 		current = new ArrayList<Integer>();
-		current.add(s);
+		current.add(1);
+	}
+	
+	public void setLineNumber(String s) throws IllegalLineException {
+		if (s.charAt(s.length()-1)=='.') {
+			throw new IllegalLineException("Extra period.");				
+		}
+		current = new ArrayList<Integer>();
+		//Makes string into a array of integers.
+			String[] temp= s.split("\\.");
+			try {	//Checks for correct format. 
+				for (int i=0; i<temp.length;i++) {
+					if (temp[i].equals("")) {
+						throw new IllegalLineException("Extra period.");				
+					}
+					current.add(Integer.parseInt(temp[i]));
+				}
+			 } catch (NumberFormatException i) { //Not integers.
+					throw new IllegalLineException("Line reference can only be numbers and periods.");	
+				 }
 	}
 
+	//Use these functions?
+	public void endSubproof() {
+		current.remove(current.size()-1); //Exit subproof.		
+		current.set(current.size()-1, current.get(current.size()-1)+1); //Next line outside of subproof.
+	}
 	
+	public void startSubproof() {
+		if (current.get(0)==1) { //Special case for step 2.
+			current.set(0, 2);
+		} else {
+			current.add(1);
+		}
+	}
+	
+	public void nextLine() { //Not starting or ending subproof.
+		current.set(current.size()-1, current.get(current.size()-1)+1);
+		}
+	
+	//Delete this?
 	public void findNextLine(String s) {
 		
 		//Needs another case for valid but unnecessary contradictions.
 		//Find nextline if completes subproof/proof.
+		
 		
 		if (s.equals("show") && current.get(0)!=1) {
 			current.add(1); //Start subproof.
@@ -29,37 +67,24 @@ public class LineNumber {
 			current.set(current.size()-1, current.get(current.size()-1)+1);
 			}
 		}
-	
-	
-	public boolean isLegalLine(String s) throws IllegalLineException {		
-		String[] temp= s.split("\\.");
-		//Correct amount of periods.
-		int counter=0;
-		for (int i=0; i<s.length(); i++) {
-			if (s.charAt(i)=='.') {
-				counter++;
-			}
+		 
+	//Check if the line array 
+	public void isAccessibleLine(LineNumber n) throws IllegalInferenceException {
+		int sizeOfN = n.current.size();
+		if (current.size()<sizeOfN) { //Line cannot be longer than current.
+			throw new IllegalInferenceException("Inaccessible line."); 
 		}
-		if (counter!=temp.length-1) {
-			throw new IllegalLineException("Line has too many periods.");
-		}
-		/*if (iAmDebugging) {
-			System.out.println(temp.length);	
-		}*/ if (current.size()<temp.length) {
-			return false; //Line cannot be longer than current.
-		}
-		//Checking that all but the last digit are the same.
-		for (int i=0; i<temp.length-1;i++) {
-			int tempInt = Integer.parseInt(temp[i]);
+		//Checking if integer except last is equal.
+		for (int i=0; i<sizeOfN-1;i++) {
 			/*if (iAmDebugging) {
-				System.out.println(tempInt + "compare" + current.get(i));
-			}*/ if (tempInt!=current.get(i)) {
-				return false; 
+				System.out.println(s[i] + "compare" + current.get(i));
+			}*/ if (n.current.get(i)!=current.get(i)) {
+				throw new IllegalInferenceException("Inaccessible line.");
 			}
-		} try {
-			return Integer.parseInt(temp[temp.length-1]) < current.get(temp.length-1);	
-		} catch (NumberFormatException i) { //Not integers.
-			throw new IllegalLineException("Line reference can only be numbers.");
+		}
+		//Last integer must be less than last integer of current line.
+		if (n.current.get(sizeOfN-1) > current.get(sizeOfN-1)) {
+			throw new IllegalInferenceException("Inaccessible line."); 		
 		}
 	}
 	
@@ -72,3 +97,4 @@ public class LineNumber {
 		return temp.substring(0,temp.length()-1); //Exclude last period.
 	}
 }
+
