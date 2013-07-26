bd1d3b4f548dde07d7330769fb1fdc3d4925860b
kellygoodman <kellyg3000@gmail.com>
2013-07-25 20:14:00 -0700

    Update ExpressionTest.java

--- src/ExpressionTest.java
+++ src/ExpressionTest.java
@@ -10,10 +10,8 @@ public class ExpressionTest extends TestCase {
 		try {
 			r = new Expression("~p");
 			//r.print();
-
 			s = new Expression("(~q&~(p=>q))");
 			//s.print();
-
 			t = new Expression("~(p=>q)");
 			//t.print();
 		} catch (IllegalLineException e) {
@@ -23,7 +21,7 @@ public class ExpressionTest extends TestCase {
 		assertFalse(thrown);
 		assertTrue(r.isOK());
 		assertTrue(s.isOK());
-		
+		assertTrue(t.isOK());
 
 		thrown = false;
 		try {
@@ -33,6 +31,7 @@ public class ExpressionTest extends TestCase {
 			assertEquals(e.getMessage(),"Error: proposition must be single lowercase letter");
 			thrown = true;
 		}
+		assertTrue(thrown);
 		
 		Expression u = new Expression();
 		thrown = false;
@@ -82,48 +81,7 @@ public class ExpressionTest extends TestCase {
 		assertTrue(thrown);
 		
 	}
-	
-	public void testSet() {
-		boolean thrown = false;
-		Expression r = new Expression();
-		Expression s = new Expression();
-		Expression t = new Expression();
-		try {
-			r = new Expression("~p");
-			r.set("p", true);
-			assertFalse(r.getRoot().getValue());
-			assertFalse(r.getRoot().getIsSet());
-			assertTrue(r.getRoot().getLeft().getValue());
-			assertTrue(r.getRoot().getLeft().getIsSet());
-			
-			r = new Expression("~p");
-			r.set("~p", true);
-			assertTrue(r.getRoot().getValue());
-			assertTrue(r.getRoot().getIsSet());
-			assertFalse(r.getRoot().getLeft().getValue());
-			assertTrue(r.getRoot().getLeft().getIsSet());
-			
-			s = new Expression("((p=>q)=>(p=>q))");
-			s.set("(p=>q)", true);
-			assertTrue(s.getRoot().getLeft().getValue());
-			assertTrue(s.getRoot().getLeft().getIsSet());
-			assertTrue(s.getRoot().getRight().getValue());	// sets all instances of (p=>q) in tree
-			assertTrue(s.getRoot().getRight().getIsSet());
-			
-			t = new Expression("~(p=>q)");
-			t.set("~(p=>q)", true);
-			assertTrue(t.getRoot().getValue());
-			assertTrue(t.getRoot().getIsSet());
-			assertFalse(t.getRoot().getLeft().getValue());
-			assertTrue(t.getRoot().getLeft().getIsSet());
 
-		} catch (IllegalLineException e) {
-			System.out.println(e.getMessage());
-			thrown = true;
-		}
-		assertFalse(thrown);
-		
-	}
 	  public void testSubExpressionEquals() {
 			try {
 				Expression e1 = new Expression("((x&y)=>x)");
